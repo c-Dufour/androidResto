929a84145cbf66ecccdad42b5a0d1e0d1ba901c3
c-dufour <duf.cyril@gmail.com>
2017-06-14 09:33:04 +0200

    ff

---
+++ app/src/main/java/com/brock/guide/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.brock.guide;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/brock/guide/MainActivity.java
@@ -0,0 +1,26 @@
+package com.brock.guide;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    private Timer myTimer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myTimer = new Timer();
+        myTimer.schedule(new TimerTask(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+    }
+}
